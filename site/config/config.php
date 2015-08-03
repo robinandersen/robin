770ce76f7cef964d0433cb2be0895cbf116ae6e8
Robin Andersen <post@robinandersen.no>
2015-08-03 18:07:20 +0200

    Kirby Cms License

--- site/config/config.php
+++ site/config/config.php
@@ -15,7 +15,7 @@ for more information: http://getkirby.com/license
 
 */
 
-c::set('license', 'put your license key here');
+c::set('license', 'K2-PERSONAL-c0cf577a408470ebfd3eb96d6a566b21');
 
 /*
 
